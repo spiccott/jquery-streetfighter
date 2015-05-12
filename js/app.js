c299d5cdbaae5792fe557a6e9378147a95b77958
Stephen Piccott <stevepiccott@gmail.com>
2015-05-12 11:50:39 -0230

    Adds mouse events to app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$(".ryu").mouseenter(function() {
+		$(".ryu-still").hide();
+		$(".ryu-ready").show();
+	})
+	.mouseleave(function() {
+		$(".ryu-ready").hide();
+		$(".ryu-still").show();
+	})
+	.mousedown(function() {
+		console.log("mouse down");
+		//play hadouken sound
+		$(".ryu-ready").hide();
+		$(".ryu-throwing").show();
+		$(".hadouken").show();
+		//show hadouken and animate it to the right of the screen
+	})
+	.mouseup(function() {
+		$(".ryu-throwing").hide();
+		$(".ryu-ready").show();
+	});
+});
